2c6bf250db2f5555f944dd3261b301052eef66d3
why-arong <pilmokim99@gmail.com>
2025-03-21 14:47:53 +0900

    feat: nowgst we can use cosmos in local dev

--- airflow-local/dags/my_dbt.py
+++ airflow-local/dags/my_dbt.py
@@ -1,28 +1,59 @@
 from datetime import datetime
+from cosmos.constants import LoadMode
+
+from airflow.operators.bash import BashOperator
+from airflow.operators.empty import EmptyOperator
 from cosmos import DbtDag, DbtTaskGroup
 from cosmos.constants import ExecutionMode
-from cosmos.config import ProfileConfig, ProjectConfig, ExecutionConfig
+from cosmos.config import ProfileConfig, ProjectConfig, ExecutionConfig, RenderConfig
 from cosmos.profiles import GoogleCloudServiceAccountDictProfileMapping
+from airflow.decorators import dag, task
+from os import environ
+
+DBT_PROJECT_PATH = f"{environ['AIRFLOW_HOME']}/dags/dbt"
 
 profile_config = ProfileConfig(
     profile_name="bigquery-db",
     target_name="dev",
     profile_mapping=GoogleCloudServiceAccountDictProfileMapping(
-        conn_id="gcp-sample"
+        conn_id="gcp-sample",
+        profile_args={"project": "goorm-bomnet", "dataset": "kma"}
     ),
 )
-my_dbt_dag = DbtDag(
-    profile_config=profile_config,
-    exection_config=ExecutionConfig(
-        execution_mode=ExecutionMode.VIRTUALENV,
-    ),
-    operator_args={
-        "py_system_site_packages": False,
-        "py_requirements": ["apache-airflow-providers-bigquery"],
-    },
-    schedule_interval="@daily",
+
+
+@dag(
     start_date=datetime(2023, 1, 1),
     catchup=False,
-    dag_id="my_cosmos_dag",
-    default_args={"retries": 1},
 )
+def my_simple_dbt_dag():
+    start_task = EmptyOperator(task_id="start-venv-examples")
+    end_task = BashOperator(
+        task_id="end-venv-examples",
+        bash_command="echo hello",
+    )
+    transform_data = DbtTaskGroup(
+        group_id="transform_data",
+        project_config=ProjectConfig(
+            dbt_project_path=DBT_PROJECT_PATH,
+            manifest_path=DBT_PROJECT_PATH + "/target/manifest.json",
+        ),
+        profile_config=profile_config,
+        execution_config=ExecutionConfig(
+            execution_mode=ExecutionMode.VIRTUALENV,
+        ),
+        render_config=RenderConfig(
+            select=["stg_mafra_kat_sale"],
+            load_method=LoadMode.DBT_MANIFEST
+        ),
+        operator_args={
+            "py_system_site_packages": False,
+            "py_requirements": ["dbt-bigquery"],  # 또는 사용하는 adapter에 맞춰 변경
+            "install_deps": True
+        }
+    )
+
+    start_task >> transform_data >> end_task
+
+
+my_simple_dbt_dag()
