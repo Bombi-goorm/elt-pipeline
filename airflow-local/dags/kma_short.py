dace76b1a73c555d724fb6c4f62915009db8f872
why-arong <pilmokim99@gmail.com>
2025-03-24 18:22:06 +0900

    fdsf

--- airflow-local/dags/kma_short.py
+++ airflow-local/dags/kma_short.py
@@ -1,6 +1,6 @@
 from airflow.decorators import dag, task
 from pendulum import datetime
-from plugins.custom_operators.data_go_abc import PublicDataToGCSOperator
+from include.custom_operators.data_go_abc import PublicDataToGCSOperator
 from airflow.providers.google.cloud.transfers.gcs_to_bigquery import GCSToBigQueryOperator
 from airflow.datasets import Dataset
 from helpers.common_utils import datago_safe_response_filter
